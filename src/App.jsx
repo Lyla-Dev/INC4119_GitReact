50c337fe7de7f9c227f7b000c2c7a5e76d892552
Minkyeong-Choi <minkyeong.choi@outlook.com>
2025-04-15 13:24:08 +0900

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   };
 
   return (
-    <div className="app">
+    <div className="appp">
       <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
       <main className="main-content">
         {!isLoggedIn ? <LoginPage onLogin={handleLogin} /> : renderPage()}
